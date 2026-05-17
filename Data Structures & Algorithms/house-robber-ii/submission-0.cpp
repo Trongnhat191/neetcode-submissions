8876071d64d06f11ba5188bb88aa6a7adb81abb0
Trongnhat191 <162899251+Trongnhat191@users.noreply.github.com>
2026-05-17 16:04:02 +0700

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob_linear(vector<int>& nums, int start, int end){
+        int n = end-start + 1;
+        vector<int> dp(n);
+        dp[0] = nums[start];
+        dp[1] = max(nums[start], nums[start+1]);
+
+        for (int i = 2; i<n; i++){
+            dp[i] = max(dp[i-1], dp[i-2] + nums[start+i]);
+        }
+        return dp[n-1];
+    }
+    int rob(std::vector<int>& nums) {
+        int n = nums.size();
+        if (n==1) return nums[0];
+        if (n==2) return max(nums[0], nums[1]);
+        
+        return max(rob_linear(nums, 0, n-2), rob_linear(nums, 1, n-1));
+    }
+};
